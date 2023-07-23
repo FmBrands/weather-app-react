54c6892784a75699d4f17448f4579d2d15c735db
FmBrands <dupegift2002@gmail.com>
2023-07-23 16:14:24 +0100

    Added axios which hep display the temperature and humidity of the city.

---
+++ src/weather.js
@@ -0,0 +1,28 @@
+import React from "react";
+import axios from "axios";
+import { FidgetSpinner } from "react-loader-spinner";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    console.log(response);
+    alert(
+      `The temperature in ${response.data.name} is ${response.data.main.temp}℃ and the humidity is ${response.data.main.humidity} %`
+    );
+  }
+  let apiKey = "d3cfc8e21373609a59380c0e9070f5be";
+  let apiUrl = ` https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+
+  return (
+    <FidgetSpinner
+      visible={true}
+      height="80"
+      width="100"
+      ariaLabel="dna-loading"
+      wrapperStyle={{}}
+      wrapperClass="dna-wrapper"
+      ballColors={["#ff0000", "#00ff00", "#0000ff"]}
+      backgroundColor="#F4442E"
+    />
+  );
+}
